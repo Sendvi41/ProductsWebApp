85383277fbf7e863d32212fe466efa3c8d62f30b
Semyon <sendvi41@gmail.com>
2020-05-20 19:54:30 +0300

    Update categories

--- src/main/java/com/gmail/sendvi41/controllers/CategoryController.java
+++ src/main/java/com/gmail/sendvi41/controllers/CategoryController.java
@@ -1,7 +1,6 @@
 package com.gmail.sendvi41.controllers;
 
 
-
 import com.gmail.sendvi41.entities.Category;
 import com.gmail.sendvi41.services.CategoryServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -20,8 +20,6 @@ public class CategoryController {
     CategoryServiceInterface categoryServiceInterface;
 
 
-
-
     @GetMapping("/listcategories")
     public String showListCategoriesPage(Model model) {
         model.addAttribute("categories", categoryServiceInterface.getCategories());
@@ -29,10 +27,9 @@ public class CategoryController {
     }
 
 
-
     @GetMapping("/addcategory")
     public String showAddCategoryPage(Model model) {
-       Category category = new Category();
+        Category category = new Category();
         model.addAttribute("category", category);
 
         return "categories/add-category";
@@ -53,5 +50,44 @@ public class CategoryController {
 
     }
 
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateFormCategory(@PathVariable("id") long id, Model model) {
+        try {
+            Category category = categoryServiceInterface.getCategory(id);
+
+            model.addAttribute("category", category);
+
+            return "categories/update-category";
+        } catch (Exception ex) {
+            return "categories/category-deleted";
+        }
+    }
+
+
+    @PostMapping("/update/{id}")
+    public String updateStudent(@PathVariable("id") long id, @ModelAttribute("category") Category category,
+                                Model model) {
+        try {
+            categoryServiceInterface.getCategory(id);
+            categoryServiceInterface.saveCategory(category);
+            return "redirect:/listcategories";
+        } catch (Exception ex) {
+            return "categories/category-deleted";
+        }
+
+
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteStudent(@PathVariable("id") long id, Model model) {
+        try {
+            categoryServiceInterface.deleteCategory(id);
+            return "redirect:/listcategories";
+        } catch (Exception ex) {
+            return "redirect:/listcategories";
+        }
+
+    }
 
 }
